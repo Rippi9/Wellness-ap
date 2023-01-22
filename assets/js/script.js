c82e88ef897f6bc2856faea64bf9da6211f48485
MLD4 <dongmiao123@gmail.com>
2023-01-22 18:26:48 +1030

    MVP basic outline

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+const dropdowns = document.querySelectorAll('.dropdown:not(.is-hoverable)');
 
+if (dropdowns.length > 0) { //required for downdowns to work (bulma)
+  dropdowns.forEach(function(el) {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      el.classList.toggle('is-active');
+    });
+  });
+  document.addEventListener('click', function(e) {
+    closeDropdowns();
+  });
+}
+
+function closeDropdowns() { //closes dropdowns when click off dropdown (bulma)
+  dropdowns.forEach(function(el) {
+    el.classList.remove('is-active');
+  });
+}
